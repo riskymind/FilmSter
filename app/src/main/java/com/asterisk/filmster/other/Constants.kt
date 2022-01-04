1ed694d33a025d06c399986d1a1f89c7a53f20ad
kelechi <oparaiskelechi@gmail.com>
2022-01-04 23:54:30 +0100

    embedded the youtube trailer player

--- app/src/main/java/com/asterisk/filmster/other/Constants.kt
+++ app/src/main/java/com/asterisk/filmster/other/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val NOW_PLAYING_URL =
         "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
     const val MOVIE_EXTRA = "MOVIE_EXTRA"
+    const val YOUTUBE_API_KEY = "AIzaSyBMU7xQpHLsin5OnnL4sUa8drV1pnVu0mQ"
+    const val TRAILERS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 }
--- app/src/main/java/com/asterisk/filmster/ui/DetailActivity.kt
+++ app/src/main/java/com/asterisk/filmster/ui/DetailActivity.kt
@@ -1,20 +1,31 @@
 package com.asterisk.filmster.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.RatingBar
 import android.widget.TextView
-import android.widget.Toast
 import com.asterisk.filmster.R
 import com.asterisk.filmster.model.Movie
 import com.asterisk.filmster.other.Constants.MOVIE_EXTRA
+import com.asterisk.filmster.other.Constants.TRAILERS_URL
+import com.asterisk.filmster.other.Constants.YOUTUBE_API_KEY
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import okhttp3.Headers
 
-class DetailActivity : AppCompatActivity() {
+
+private const val TAG = "DetailActivity"
+
+class DetailActivity : YouTubeBaseActivity() {
 
     private lateinit var tvTitle: TextView
     private lateinit var tvOverview: TextView
     private lateinit var ratingBar: RatingBar
-
+    private lateinit var ytPlayer: YouTubePlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +34,57 @@ class DetailActivity : AppCompatActivity() {
         tvTitle = findViewById(R.id.tv_title)
         tvOverview = findViewById(R.id.tv_overview)
         ratingBar = findViewById(R.id.rb_rate)
+        ytPlayer = findViewById(R.id.player)
 
         val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
 
         tvTitle.text = movie.title
         tvOverview.text = movie.overview
         ratingBar.rating = movie.voteAverage.toFloat()
+
+        val client = AsyncHttpClient()
+        client.get(TRAILERS_URL.format(movie.movieId), object : JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                val results = json.jsonObject.getJSONArray("results")
+                if (results.length() == 0) {
+                    return
+                }
+
+                val movieTrailerJson = results.getJSONObject(0)
+                val youTubeKey = movieTrailerJson.getString("key")
+                initializeYoutubePlayer(youTubeKey)
+            }
+
+        })
+
+    }
+
+    private fun initializeYoutubePlayer(youTubeKey: String) {
+        ytPlayer.initialize(YOUTUBE_API_KEY, object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                p1?.cueVideo(youTubeKey)
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
